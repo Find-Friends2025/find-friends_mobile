7c54f8a5163147f5e7ca9ec815988523ed47e870
신민호 <68156836+alsh0807@users.noreply.github.com>
2025-04-14 19:26:17 +0900

    edit theme

--- lib/ui/core/themes/colors.dart
+++ lib/ui/core/themes/colors.dart
@@ -13,36 +13,36 @@ class DGColors {
 class _DGColorsStatic {
   _DGColorsStatic._();
 
-  static Color get white => const Color(0xffFFFFFF);
-  static Color get black => const Color(0xff000000);
-  static Color get negative => const Color(0xffFF1F4C);
-  static Color get positive => const Color(0xff20DF33);
+  Color get white => const Color(0xffFFFFFF);
+  Color get black => const Color(0xff000000);
+  Color get negative => const Color(0xffFF1F4C);
+  Color get positive => const Color(0xff20DF33);
 
 }
 
 class _DGColorsLabel {
   _DGColorsLabel._();
 
-  static Color get normal => const Color(0xff0C0C0D);
-  static Color get strong => const Color(0xff000000);
-  static Color get neutral => const Color(0xff3B3B40);
-  static Color get alternative => const Color(0xff5E5E66);
-  static Color get assistive => const Color(0xff5E5E66);
-  static Color get disable => const Color(0xffF6F6F7);
+  Color get normal => const Color(0xff0C0C0D);
+  Color get strong => const Color(0xff000000);
+  Color get neutral => const Color(0xff3B3B40);
+  Color get alternative => const Color(0xff5E5E66);
+  Color get assistive => const Color(0xff5E5E66);
+  Color get disable => const Color(0xffF6F6F7);
 
 }
 
 class _DGColorsLine {
   _DGColorsLine._();
 
-  static Color get normal => const Color(0xffE4E4E5);
-  static Color get neutral => const Color(0xffF2F2F3);
-  static Color get alternative => const Color(0xffF6F6F7);
+  Color get normal => const Color(0xffE4E4E5);
+  Color get neutral => const Color(0xffF2F2F3);
+  Color get alternative => const Color(0xffF6F6F7);
 }
 
 class _DGColorsBackground {
   _DGColorsBackground._();
 
-  static Color get normal => const Color(0xffFFFFFF);
-  static Color get neutral => const Color(0xffFCFCFD);
+  Color get normal => const Color(0xffFFFFFF);
+  Color get neutral => const Color(0xffFCFCFD);
 }
--- lib/ui/core/themes/theme.dart
+++ lib/ui/core/themes/theme.dart
@@ -1,6 +1,16 @@
 import 'package:find_friends/ui/core/themes/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 ThemeData get DGTheme => ThemeData(
-  scaffoldBackgroundColor: DGColors.background.
+  scaffoldBackgroundColor: DGColors.background.normal,
+  textSelectionTheme: TextSelectionThemeData(
+      cursorColor: DGColors.static.black,
+      selectionColor: DGColors.static.black.withOpacity(0.2),
+      selectionHandleColor: DGColors.static.black
+  ),
+  cupertinoOverrideTheme: NoDefaultCupertinoThemeData(
+      primaryColor: DGColors.static.black,
+      applyThemeToAll: true
+  ),
 );
