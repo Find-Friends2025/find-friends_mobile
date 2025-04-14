4c4a830d3bb5191f446177ce045fe480cabf776d
신민호 <68156836+alsh0807@users.noreply.github.com>
2025-04-14 19:41:39 +0900

    icons

---
+++ lib/ui/core/themes/icons.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+enum DGIcons {
+  check;
+}
+
+extension DGIconsImage on DGIcons {
+  SvgPicture get image => toImage();
+
+
+  SvgPicture toImage({double? width, double? height, Color color = const Color(0xff000000)}) {
+    return SvgPicture.asset("assets/icons/${name}.svg",
+      width: width,
+      height: height,
+      fit: BoxFit.fitHeight,
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+    );
+  }
+}
