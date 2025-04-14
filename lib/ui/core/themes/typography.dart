3f531ec00fc734f26b33851f1a36627e133b8370
8954sood <8954sood@naver.com>
2025-04-14 17:33:50 +0900

    feat: Create Typography

---
+++ lib/ui/core/themes/typography.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTypography {
+  static const display1Bold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 36,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const display1Medium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 36,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const display1Regular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 36,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const display2Bold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const display2Medium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const display2Regular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const title1Bold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const title1Medium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const title1Regular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const title2Bold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const title2Medium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const title2Regular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const headline1Bold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const headline1Medium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const headline1Regular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const headline2Bold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const headline2Medium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const headline2Regular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const bodyBold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const bodyMedium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const bodyRegular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const labelBold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const labelMedium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const labelRegular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const captionBold = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static const captionMedium = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+  static const captionRegular = TextStyle(
+    fontFamily: "pretendard",
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+}
